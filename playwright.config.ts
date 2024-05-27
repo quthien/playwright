261dffe52d625279f628e6baf3247ab080541a8a
Thien Nguyen <thien.vincent1499@gmail.com>
2024-05-28 00:03:43 +0700

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ import { defineConfig, devices } from "@playwright/test";
  */
 export default defineConfig({
   testDir: "./tests",
-  reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
+  reporter: [["list", { outputFolder: "playwright-report", open: "never" }]],
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
   expect: {
