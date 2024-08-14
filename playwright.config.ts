708fc81ce1b24ac01b73ccf35ee33b9011b04525
Thien Nguyen <thien.vincent1499@gmail.com>
2024-08-15 00:14:28 +0700

    update

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,8 @@
 import { LaunchOptions } from "@playwright/test";
-
+const isHeadless = process.env.HEADLESS === "true" ? true : false;
 const browserOptions: LaunchOptions = {
   slowMo: 0,
-  headless: process.env.HEADLESS === "false", // Run tests in headless mode
+  headless: isHeadless, // Run tests in headless mode
   timeout: 60000,
 };
 
